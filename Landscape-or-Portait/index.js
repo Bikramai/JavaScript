43be171fa3b21f6ce56a4905528fe03cc1c969fa
bikramia <bikram.rhce@gmail.com>
2024-10-15 21:44:32 -0400

    Landscape or Portait Project Completed

--- Landscape-or-Portait/index.js
+++ Landscape-or-Portait/index.js
@@ -5,3 +5,10 @@ width, and fheight of an image, and returns true,
 if the image is landscape, which means width is greater than height, 
 otherwise it returns false. 
 */
+
+console.log(isLandsxape(300, 600));
+
+
+function isLandsxape(width, height) {
+    return (width > height);
+}
