f7185d28b9663ca653df931b6a7bd8f230bb0f7b
bikramia <bikram.rhce@gmail.com>
2024-10-16 21:35:22 -0400

    added mapping logic way steps

--- Landscape-or-Portait/index.js
+++ Landscape-or-Portait/index.js
@@ -6,7 +6,24 @@ if the image is landscape, which means width is greater than height,
 otherwise it returns false. 
 */
 
-console.log(isLandsxape(300, 600));
+
+// Solution:
+// Logic Mapping approch
+// declare function & make parameters width and height
+// make if & else statement. inside parameters, - (width > height)
+// return true or return false.  
+// return (w > h)
+// add conditional operator on same line with true : false, 
+
+function isLandsxape(width, height) {
+    if (width > height) return true;
+        return false; OR (belos)
+}
+
+
+// Optimized way
+
+console.log(isLandsxape(800, 600));
 
 
 function isLandsxape(width, height) {
