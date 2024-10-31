897254a9ca94ccb3d903301ca4945454e9ba655c
bikramia <bikram.rhce@gmail.com>
2024-10-30 20:45:57 -0400

    Project -Even and Odd Number best way

--- Even-and-Odd-Numbers/index.js
+++ Even-and-Odd-Numbers/index.js
@@ -8,9 +8,9 @@ function showNumbers(limit) {
 }
 
 // Best way of practice(optimized way)
-// function showNumbers(limit) {
-//   for (let i = 0; i <= limit; i++) {
-//     const message = i % 2 === 0 ? "EVEN" : "ODD";
-//     console.log(i, message);
-//   }
-// }
+function showNumbers(limit) {
+  for (let i = 0; i <= limit; i++) {
+    const message = i % 2 === 0 ? "EVEN" : "ODD";
+    console.log(i, message);
+  }
+}
